2d864a89c4ffceb3650aa6996e72451e98c77897
Huanji Sun <huanjisun@Huanjis-MacBook-Pro.local>
2023-04-25 14:50:57 +0900

    Add sample

---
+++ launchers/data-factory-sample/consumer/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    `java-library`
+    id("application")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+val groupId: String by project
+val edcVersion: String by project
+
+dependencies {
+    implementation("$groupId:control-plane-core:$edcVersion")
+    implementation("$groupId:http:$edcVersion")
+    implementation("$groupId:management-api:$edcVersion")
+    implementation("$groupId:api-observability:$edcVersion")
+    implementation("$groupId:configuration-filesystem:$edcVersion")
+
+    // API key authentication for Data Management API
+    implementation("$groupId:auth-tokenbased:$edcVersion")
+
+    implementation("$groupId:iam-mock:$edcVersion") //For identity service
+    implementation("$groupId:vault-azure:$edcVersion")
+
+//    No provider dispatcher registered for protocol: ids-multipart
+    implementation("$groupId:ids:$edcVersion") //IDS Multipart Dispatcher API, IDS Transform Extension
+
+    implementation("$groupId:provision-blob:$edcVersion")
+}
+
+application {
+    mainClass.set("org.eclipse.edc.boot.system.runtime.BaseRuntime")
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    exclude("**/pom.properties", "**/pom.xm")
+    mergeServiceFiles()
+    dependsOn(":consumer:jar")
+    dependsOn(":consumer:distTar")
+    dependsOn(":consumer:distZip")
+    archiveFileName.set("consumer.jar")
+}
+
---
+++ launchers/data-factory-sample/provider/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    `java-library`
+    id("application")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+val groupId: String by project
+val edcVersion: String by project
+
+dependencies {
+    implementation("$groupId:control-plane-core:$edcVersion")
+    implementation("$groupId:http:$edcVersion")
+//    implementation("$groupId:auth-tokenbased:$edcVersion")
+    implementation("$groupId:management-api:$edcVersion")
+    implementation("$groupId:api-observability:$edcVersion")
+    implementation("$groupId:configuration-filesystem:$edcVersion")
+    //Catalog
+    implementation("$groupId:ids:$edcVersion")
+    //IAM
+    implementation("$groupId:iam-mock:$edcVersion")
+    implementation("$groupId:vault-azure:$edcVersion")
+    //Auth
+    implementation("$groupId:auth-tokenbased:$edcVersion")
+    //Transfer Data
+    implementation("$groupId:data-plane-core:$edcVersion")
+    implementation("$groupId:azure-resource-manager:$edcVersion")
+    implementation(project(":Connector:extensions:data-plane:data-plane-azure-data-factory"))
+
+    implementation("$groupId:data-plane-selector-client:$edcVersion")
+    implementation("$groupId:data-plane-selector-core:$edcVersion")
+    implementation("$groupId:transfer-data-plane:$edcVersion")
+}
+
+application {
+    mainClass.set("org.eclipse.edc.boot.system.runtime.BaseRuntime")
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    exclude("**/pom.properties", "**/pom.xm")
+    mergeServiceFiles()
+    dependsOn(":provider:jar")
+    dependsOn(":provider:distTar")
+    dependsOn(":provider:distZip")
+    archiveFileName.set("provider.jar")
+}
